b62cae274964b931ba15cd2051057f874621fd68
Matt Belland <kristopher.belland@students.olin.edu>
2011-02-02 19:30:39 -0500

    Attempting to add GoogleApps login support.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,8 +3,9 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'muBfbtD5at102zlPpr61g', 'nCrSgkjEYtNcC0wbadvITgalGsJx34BpEbt10'
+  provider :googleapps, 'stormy-light-324.heroku.com', 'UxIb7thTkLvkJbHfLDDoQqfp'
 end
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '185585381473058', '48a45a996da51e9ec7010310c8925d84'
-end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, '185585381473058', '48a45a996da51e9ec7010310c8925d84'
+# end
